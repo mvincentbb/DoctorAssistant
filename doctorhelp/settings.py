a8d9159cd96b3ff5fc42b60d1d4be44a12b66fae
mvincentbb2020 <mvincentbb@gmail.com>
2020-07-02 23:56:15 +0100

    adding sqllite3

--- doctorhelp/settings.py
+++ doctorhelp/settings.py
@@ -77,18 +77,18 @@ WSGI_APPLICATION = 'doctorhelp.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         # 'ENGINE': 'django.db.backends.postgresql',
-#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': 'doctordb',
 #         'USER': 'postgres',
 #         'PASSWORD': 'admin',
 #         'HOST': 'localhost',
 #         'PORT': '5432',
-#     }
-# }
+    }
+}
 
 
 # Password validation
