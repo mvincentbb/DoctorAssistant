c413a5660ef75d06906dbf01642ccecd7cd260a4
Samtou <christophe1999da@gmail.com>
2020-07-25 12:11:26 +0000

    STATICFILES_DIRS

--- doctorhelp/settings.py
+++ doctorhelp/settings.py
@@ -143,7 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # CORS_ORIGIN_WHITELIST = ""
 
